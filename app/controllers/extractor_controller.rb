ef3aa95115879ec0a4848642e07f7c2cf6de92a0
kessystaub <kessystaub@outlook.com>
2024-11-30 12:06:51 -0300

    First commit

---
+++ app/controllers/extractor_controller.rb
@@ -0,0 +1,2 @@
+class ExtractorController < ApplicationController
+end
---
+++ test/controllers/extractor_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ExtractorControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
